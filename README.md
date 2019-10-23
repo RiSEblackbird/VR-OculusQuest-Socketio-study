031c4017de8da166d84894eeab3f81ace777b012
TAISHI SENOH <mokut2017@gmail.com>
2019-10-23 16:56:58 +0900

    ADD JS. index.js : 通信処理の記述 ①プレイヤー接続, ②ゲームプレイ中の通信

--- README.md
+++ README.md
@@ -1,29 +1,50 @@
 # VR-OculusQuest-Socketio-study  
 
+基本的に下記掲載の動画のチュートリアルに沿って進める。
+
 ## Reference tutorial
 ### **Unity 5 Multiplayer Networking Node JS socket.io Pt1**  
  - イントロダクション  
-https://www.youtube.com/watch?v=tn3cWcSYmHE  
+ https://www.youtube.com/watch?v=tn3cWcSYmHE  
   
 ### **Unity 5 Multiplayer Networking Node JS socket.io Pt4**  
  - リアルタイム通信におけるプレイヤー移動の定義  
-https://www.youtube.com/watch?v=Vqdlrky4cTM  
+ https://www.youtube.com/watch?v=Vqdlrky4cTM  
 
 ### **~Pt7**  
  - ネットワークマネージャー  
-https://www.youtube.com/watch?v=0jmUQ0ErAyU  
+ https://www.youtube.com/watch?v=0jmUQ0ErAyU  
 
 ### **~Pt8**
  - 各種JSONの定義(プレイヤーや敵の位置、体力、射撃など)　※現状自環境ではプレイヤーの位置関係のみ実装。
  https://www.youtube.com/watch?v=oYucBRfjyRE  
 
+### **~Pt9**
+ - node.js, expressでゲームサーバーを立てる
+ https://www.youtube.com/watch?v=HKyl2wSShFM
+
 ## Reference articles  
 **C#, IEnumeratorとIEnumerableを調べた**  
-https://qiita.com/vc_kusuha/items/2048391d821cb94fa489  
+ https://qiita.com/vc_kusuha/items/2048391d821cb94fa489  
 
 **json parserとは？オンラインパーサやPython、JSでの書き方を解説！**  
-https://www.sejuku.net/blog/79898  
+ https://www.sejuku.net/blog/79898  
 
 **Unity, JsonUtilityでJsonへシリアライズしたデータをローカルに保存する**  
-https://hiyotama.hatenablog.com/entry/2019/08/30/110000  
+ https://hiyotama.hatenablog.com/entry/2019/08/30/110000  
+
+**JavaScriptのjson stringifyを完全に理解しよう！**  
+ https://www.sejuku.net/blog/79911
+
+**JavaScript入門】pushで配列に要素を追加する方法(連想配列/pop)**  
+ https://www.sejuku.net/blog/84475  
+
+**JS Math.random()**  
+ https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Math/random  
+
+**JS Math**  
+ https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Math  
+
+**Unique id generator in javascript**  
+ https://learnersbucket.com/examples/javascript/unique-id-generator-in-javascript/  
 
---
+++ nodeGameServer/index.js
@@ -0,0 +1,82 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(3000);
+
+var enemies = [];
+var playerSpawnerPoints = [];
+var clients = [];
+
+app.get('/', function(req, res) {
+    res.send('ホーム');
+});
+
+// 通信について
+io.on('connection', function(socket){
+
+    var currentPlyer = {};
+    currentPlayer.neme = 'unknown';
+
+    // ①Player接続
+    socket.on('player connect', function(){
+        console.log(currentPlayer.name + ' recv: player connect');
+        // すべてのクライアントについて名前と位置と回転を定義
+        for(var i; i<clients.length; i++) {
+            var playerConnected = {
+                name:clients[i].name,
+                position:clients[i].position,
+                rotation:clients[i].rotation
+            };
+            socket.emit('other player connected', playerConnected);
+            // 接続された」プレイヤーの情報を表示
+            console.log(currentPlayer.name + ' emit: other player connected: ' + JSON.stringify(playerConnected));
+        }
+    });
+
+    // ②ゲームプレイ中の通信
+    socket.on('play', function(data){
+        console.log(currentPlayer.name + 'recv: play: ' + JSON.stringify(data));
+
+        // 既存のクライアントがいない場合
+        if(clients.length == 0) {
+            playerSpawnerPoints = [];
+            data.playerSpawnerPoints.forEach(function(_playerSpawnPoint) {
+                var playerSpawnerPoint = {
+                    position: _playerSpawnPoint.position,
+                    rotation: _playerSpawnPoint.rotation
+                };
+                playerSpawnerPoints.push(playerSpawnerPoint);
+            });
+        }
+
+        var enemiesResponse = {
+            enemies: enemies
+        };
+
+        console.log(currentPlayer.name + ' emit: enemies: ' + JSON.stringify(enemiesResponse));
+        socket.emit('enemies', enemiesResponse);
+        var randamSpawnPoint = playerSpawnerPoints[Math, floor(Math.ramdom() * playerSpawnerPoints.length)];
+        currentPlayer = {
+            name: data.name,
+            position: randamSpawnPoint.position,
+            rotation: randamSpawnPoint.rotation
+        };
+        // クライアントの列に現在接続したプレイヤーを加える
+        clients.push(currentPlayer);
+        // 現在入っているプレイヤーを出力
+        console.log(currentPlyer.name + ' emit: play: ' + JSON.stringify(currentPlyer));
+        // 既にいるプレイヤー達に自分が入ったことを伝える
+        socket.broadcast.emit('other player connected', currentPlyer);
+    });
+});
+
+console.log('server is running...' + guid());
+
+function guid() {
+    function s4() {
+        // 乱数でランダムな数字を取得して英数字の文字列を生成する
+        return Math.floor((1+Math.random()) * 0x10000).toString(16).substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +s4() +s4() + s4();
+}
