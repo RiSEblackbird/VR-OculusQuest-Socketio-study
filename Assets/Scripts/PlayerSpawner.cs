70d528509b6fe999c60ee347e6717a46730ceb98
TAISHI SENOH <mokut2017@gmail.com>
2019-10-31 21:06:38 +0900

    FIX CS. PlayerSpawner : スポーン位置のランダム化を断念。エディターで１点のみ指定する。

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class PlayerSpawner : MonoBehaviour
 {
     public GameObject player;
-    public SpawnPoint playerSpawnPoint;
-    public int numberOfPoint;
+    public int numberOfPoint = 15;
     [HideInInspector]
     public List<SpawnPoint> SpawnPoints;
+    public SpawnPoint playerSpawnPoint;
 
+    /* スポーン位置ランダム：今後余裕があれば。
     public void GenerateSpownPoint()
     {
-        playerSpawnPoint = SpawnPoints[Random.Range(0, SpawnPoints.Count)];
+        playerSpawnPoint = SpawnPoints[Random.Range(1, SpawnPoints.Count)];
         for (int i = 0; i < numberOfPoint; i++)
         {
-            var spawnPosition = new Vector3(Random.Range(-30f, 30f), Random.Range(10f, 30f), Random.Range(-30f, 30f));
+            var spawnPosition = new Vector3(Random.Range(-10f, 10f), 0, Random.Range(-10f, 10f));
             var spawnRotation = Quaternion.Euler(0f, Random.Range(0, 180), 0f);
             SpawnPoint PlayerSpawnPoint = (Instantiate(player, spawnPosition, spawnRotation) as GameObject).GetComponent<SpawnPoint>();
             SpawnPoints.Add(PlayerSpawnPoint);
         }
     }
+    */
 }
